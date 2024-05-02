220287862a3b2b44fa3361164b377970b98b1395
root <Emmydapson>
2024-05-02 10:23:25 -0700

    test cases created for models

---
+++ README.md
@@ -0,0 +1,16 @@
+Welcome to AirBnB Clone Project! 🏡🌍
+Welcome to our repository! Here, you'll find a direct clone version of the world's renowned and dynamic platform for property rentals. Our aim is to provide seamless accommodation experiences, offering everything from cozy cabins to luxurious villas. With our user-friendly interface, we strive to ensure an unforgettable stay for every user.
+
+Features
+1.Wide Range of Properties: Explore a diverse selection of properties, catering to various tastes and preferences.
+2.User-Friendly Interface: Our intuitive interface makes browsing, booking, and managing your accommodations a breeze.
+3.Secure Payment System: Rest assured, your payments are processed securely, offering peace of mind throughout your booking process.
+4.Instant Booking: Enjoy the convenience of instant booking for select properties, ensuring a hassle-free experience.
+5.24/7 Customer Support: Our dedicated support team is available around the clock to assist you with any inquiries or issues you may encounter.
+
+Getting Started
+To get started with our AirBnB Clone:
+
+Clone this repository to your local machine.
+Install the necessary dependencies.
+Run the application and start exploring properties.
---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""BaseModel: Defines a base model class with common attributes/methods"""
+
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """BaseModel class defines common attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of BaseModel"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
+
