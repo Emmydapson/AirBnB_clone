c53c34fd664d3b92d4742d0732f37476e070f41b
root <Emmydapson>
2024-05-02 11:30:26 -0700

    created the models instances

---
+++ models/engine/file_storage.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""This module defines the class FileStorage"""
+
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+class FileStorage:
+    """This class serializes instances to a JSON file and deserializes JSON
+        file to instances"""
+
+    __file_path = "file.json"  # path to the JSON file
+    __objects = {}  # dictionary that stores all objects by <class name>.id
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        new_dict = {key: value.to_dict() for key, value in self.__objects.items()}
+        with open(self.__file_path, "w") as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects (only if the JSON file
+            (__file_path) exists; otherwise, do nothing."""
+        try:
+            with open(self.__file_path, "r") as f:
+                new_dict = json.load(f)
+            class_dict = {
+                "BaseModel": BaseModel,
+                "User": User,
+                "State": State,
+                "City": City,
+                "Place": Place,
+                "Amenity": Amenity,
+                "Review": Review,
+            }
+            self.__objects = {
+                key: class_dict[value["__class__"]](**value) for key, value in new_dict.items()
+            }
+        except FileNotFoundError:
+            pass
+
